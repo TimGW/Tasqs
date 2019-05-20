a1c7569d0a1ff730e85c52bcfde3ac7ed09d05a7
timgortworst <tim.gortworst@nos.nl>
2019-05-20 19:08:42 +0200

    repeating events

--- app/src/main/java/com/timgortworst/roomy/utils/TimeExtensions.kt
+++ app/src/main/java/com/timgortworst/roomy/utils/TimeExtensions.kt
@@ -3,5 +3,5 @@ package com.timgortworst.roomy.utils
 import android.text.format.DateUtils
 
 fun Long.isTimeStampInPast(): Boolean {
-    return DateUtils.isToday(this + DateUtils.DAY_IN_MILLIS) && this < System.currentTimeMillis()
+    return /*DateUtils.isToday(this + DateUtils.DAY_IN_MILLIS) && */ this < System.currentTimeMillis()
 }
