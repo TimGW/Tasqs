e84b987185c4806f3ba9e94d9b70531aaa552a8f
timgortworst <tim.gortworst@nos.nl>
2019-07-12 21:40:24 +0200

    context menu list

--- app/src/main/java/com/timgortworst/roomy/ui/agenda/view/AgendaFragment.kt
+++ app/src/main/java/com/timgortworst/roomy/ui/agenda/view/AgendaFragment.kt
@@ -2,14 +2,22 @@ package com.timgortworst.roomy.ui.agenda.view
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.view.*
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.timgortworst.roomy.R
 import com.timgortworst.roomy.model.Event
 import com.timgortworst.roomy.ui.agenda.adapter.EventListAdapter
 import com.timgortworst.roomy.ui.agenda.presenter.AgendaPresenter
 import com.timgortworst.roomy.ui.main.view.MainActivity
-import com.timgortworst.roomy.ui.settings.view.SettingsActivity
+import com.timgortworst.roomy.utils.RecyclerTouchListener
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_agenda.*
@@ -17,6 +25,7 @@ import javax.inject.Inject
 
 
 class AgendaFragment : androidx.fragment.app.Fragment(), AgendaView {
+    private lateinit var touchListener: RecyclerTouchListener
     private lateinit var activityContext: AppCompatActivity
     private var listeningToEvents: Boolean = false
     private lateinit var adapter: EventListAdapter
@@ -25,8 +34,6 @@ class AgendaFragment : androidx.fragment.app.Fragment(), AgendaView {
     lateinit var presenter: AgendaPresenter
 
     companion object {
-        private val TAG = AgendaFragment::class.java.simpleName
-
         fun newInstance(): AgendaFragment {
             return AgendaFragment()
         }
@@ -55,6 +62,7 @@ class AgendaFragment : androidx.fragment.app.Fragment(), AgendaView {
         swipe_container.isEnabled = false
 
         setupEventListAdapter()
+        setupRecyclerContextMenu()
 
         if (!listeningToEvents) {
             presenter.listenToEvents()
@@ -66,6 +74,7 @@ class AgendaFragment : androidx.fragment.app.Fragment(), AgendaView {
         super.onResume()
         activityContext.supportActionBar?.title = getString(R.string.schema_toolbar_title)
         activityContext.fab.setOnClickListener { EditAgendaEventActivity.start(activityContext) }
+        events_agenda.addOnItemTouchListener(touchListener)
     }
 
     override fun onPause() {
@@ -75,12 +84,35 @@ class AgendaFragment : androidx.fragment.app.Fragment(), AgendaView {
 
     private fun setupEventListAdapter() {
         adapter = EventListAdapter(activityContext, mutableListOf())
-        val layoutManager = androidx.recyclerview.widget.LinearLayoutManager(activityContext)
+        val layoutManager = LinearLayoutManager(activityContext)
         events_agenda.layoutManager = layoutManager
         val dividerItemDecoration =
-            androidx.recyclerview.widget.DividerItemDecoration(events_agenda.context, layoutManager.orientation)
+                DividerItemDecoration(events_agenda.context, layoutManager.orientation)
         events_agenda.addItemDecoration(dividerItemDecoration)
         events_agenda.adapter = adapter
+    }
+
+    private fun setupRecyclerContextMenu() {
+        touchListener = RecyclerTouchListener(activityContext, events_agenda)
+        touchListener
+                .setClickable(object : RecyclerTouchListener.OnRowClickListener {
+                    override fun onRowClicked(position: Int) {
+                        Toast.makeText(activityContext, adapter.getEventList()[position].user.name, Toast.LENGTH_SHORT)
+                                .show()
+                    }
+                    override fun onIndependentViewClicked(independentViewID: Int, position: Int) { }
+                })
+                .setSwipeOptionViews(R.id.delete_task, R.id.edit_task)
+                .setSwipeable(R.id.rowFG, R.id.rowBG) { viewID, position ->
+                    when (viewID) {
+                        R.id.delete_task -> {
+                            adapter.removeEvent(position)
+                        }
+                        R.id.edit_task -> {
+                            // todo
+                        }
+                    }
+                }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
