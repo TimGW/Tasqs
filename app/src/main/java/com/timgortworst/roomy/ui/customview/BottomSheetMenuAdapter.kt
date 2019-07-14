c31e39781b2cef788a4f344792afa57b8b2552cb
timgortworst <tim.gortworst@nos.nl>
2019-07-14 23:28:09 +0200

    cleanup

--- app/src/main/java/com/timgortworst/roomy/ui/customview/BottomSheetMenuAdapter.kt
+++ app/src/main/java/com/timgortworst/roomy/ui/customview/BottomSheetMenuAdapter.kt
@@ -1,15 +1,15 @@
 package com.timgortworst.roomy.ui.customview
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.timgortworst.roomy.R
 import com.timgortworst.roomy.model.BottomMenuItem
 import kotlinx.android.synthetic.main.item_bottom_sheet_menu.view.*
 
 class BottomSheetMenuAdapter(private val items: List<BottomMenuItem>) :
-    androidx.recyclerview.widget.RecyclerView.Adapter<BottomSheetMenuAdapter.BottomSheetMenuViewHolder>() {
+    RecyclerView.Adapter<BottomSheetMenuAdapter.BottomSheetMenuViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BottomSheetMenuViewHolder {
         return BottomSheetMenuViewHolder(
@@ -27,7 +27,7 @@ class BottomSheetMenuAdapter(private val items: List<BottomMenuItem>) :
         holder.bind(items[position])
     }
 
-    class BottomSheetMenuViewHolder(val view: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(view) {
+    class BottomSheetMenuViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
         fun bind(item: BottomMenuItem) {
             with(view) {
