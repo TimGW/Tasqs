e18a005d6d6e51673d2db14dc97c263d7eaade8d
timgortworst <tim.gortworst@nos.nl>
2019-07-14 20:09:46 +0200

    repeat icon

---
+++ app/src/main/java/com/timgortworst/roomy/ui/customview/RepeatIcon.kt
@@ -0,0 +1,32 @@
+package com.timgortworst.roomy.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.RelativeLayout
+import com.timgortworst.roomy.R
+import com.timgortworst.roomy.model.EventMetaData
+import com.timgortworst.roomy.model.EventMetaData.RepeatingInterval.*
+import kotlinx.android.synthetic.main.custom_repeat_icon.view.*
+
+class RepeatIcon
+@JvmOverloads
+constructor(context: Context,
+            attrs: AttributeSet? = null,
+            defStyleAttr: Int = 0) : RelativeLayout(context, attrs, defStyleAttr) {
+    init {
+        LayoutInflater.from(context).inflate(R.layout.custom_repeat_icon, this, true)
+    }
+
+    fun setRepeatLabelText(interval: EventMetaData.RepeatingInterval) {
+        val label = when (interval) {
+            SINGLE_EVENT -> ""
+            DAILY -> context.getString(R.string.repeat_label_interval_text_day)
+            WEEKLY -> context.getString(R.string.repeat_label_interval_text_week)
+            MONTHLY -> context.getString(R.string.repeat_label_interval_text_month)
+            ANNUALLY -> context.getString(R.string.repeat_label_interval_text_year)
+        }
+        event_repeat_text.text = label.toUpperCase()
+    }
+}
+
