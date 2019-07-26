1ee2674223fb461574ea8051dbad46d6cc00779e
timgortworst <tim.gortworst@nos.nl>
2019-07-26 19:52:46 +0200

    refactor to usecases

---
+++ app/src/main/java/com/timgortworst/roomy/domain/UserListInteractor.kt
@@ -0,0 +1,64 @@
+package com.timgortworst.roomy.domain
+
+import com.timgortworst.roomy.model.Household
+import com.timgortworst.roomy.model.User
+import com.timgortworst.roomy.repository.EventRepository
+import com.timgortworst.roomy.repository.HouseholdRepository
+import com.timgortworst.roomy.repository.UserRepository
+import com.timgortworst.roomy.utils.Constants.USER_HOUSEHOLDID_REF
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class UserListInteractor
+@Inject
+constructor(private val householdRepository: HouseholdRepository,
+            private val userRepository: UserRepository,
+            private val eventRepository: EventRepository) {
+
+    suspend fun getCurrentUser() = userRepository.getCurrentUser()
+
+    fun getCurrentUserId() = userRepository.getCurrentUserId()
+
+    suspend fun getHouseholdIdForCurrentUser() = userRepository.getHouseholdIdForCurrentUser()
+
+    suspend fun getUsersForHouseholdId(householdId: String) = userRepository.getUsersForHouseholdId(householdId)
+
+    suspend fun deleteAndBanUser(user: User) {
+        // set user on blacklist
+        addUserToBlackList(user.userId)
+
+        // remove id from user document
+        userRepository.userCollectionRef
+                .document(user.userId)
+                .update(USER_HOUSEHOLDID_REF, "")
+                .await()
+
+        // remove all events for that user
+        removeEventsForUser(user.userId)
+    }
+
+    private suspend fun addUserToBlackList(userId: String) {
+        val household = householdRepository.householdsCollectionRef
+                .document(userRepository.getHouseholdIdForUser(userId))
+                .get()
+                .await()
+                .toObject(Household::class.java) as Household
+
+        household.blackList.add(userId)
+        householdRepository.updateHousehold(household.householdId, household.blackList)
+    }
+
+    private suspend fun removeEventsForUser(userId: String) {
+//        val householdRef = householdRepository.householdsCollectionRef
+//                .document(userRepository.getHouseholdIdForUser(userId))
+//
+//        val event = householdRef.collection(EVENT_COLLECTION_REF).document().whereEqualTo("user.userId", userId).get() as Event
+//
+//        householdRef
+//            .collection(EVENT_COLLECTION_REF)
+//            .document(event.)
+//            .delete()
+//            .await()
+    }
+
+}
--- app/src/main/java/com/timgortworst/roomy/ui/user/presenter/UserListPresenter.kt
+++ app/src/main/java/com/timgortworst/roomy/ui/user/presenter/UserListPresenter.kt
@@ -2,9 +2,9 @@ package com.timgortworst.roomy.ui.user.presenter
 
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import com.timgortworst.roomy.domain.UserListInteractor
 import com.timgortworst.roomy.model.Role
 import com.timgortworst.roomy.model.User
-import com.timgortworst.roomy.repository.UserRepository
 import com.timgortworst.roomy.ui.user.view.UserListView
 import com.timgortworst.roomy.utils.CoroutineLifecycleScope
 import kotlinx.coroutines.Dispatchers
@@ -13,7 +13,7 @@ import javax.inject.Inject
 
 class UserListPresenter @Inject constructor(
     private val view: UserListView,
-    private val userRepository: UserRepository
+    private val userListInteractor: UserListInteractor
 ) : DefaultLifecycleObserver {
 
     private val scope = CoroutineLifecycleScope(Dispatchers.Main)
@@ -25,24 +25,24 @@ class UserListPresenter @Inject constructor(
     }
 
     fun fetchUsers() = scope.launch {
-        val userList = userRepository
-            .getUsersForHouseholdId(userRepository.getHouseholdIdForCurrentUser())
+        val userList = userListInteractor
+            .getUsersForHouseholdId(userListInteractor.getHouseholdIdForCurrentUser())
             .sortedWith(compareBy({ it.role == Role.ADMIN.name }, { it.name }))
             .reversed()
             .toMutableList()
 
-        val currentUser = userList.find { it.userId == userRepository.getCurrentUserId() }
+        val currentUser = userList.find { it.userId == userListInteractor.getCurrentUserId() }
         view.showOrHideFab(userList.size < 8 && currentUser?.role == Role.ADMIN.name)
         view.presentUserList(userList)
     }
 
     fun deleteUser(user: User) = scope.launch {
-        userRepository.deleteUser(user)
+        userListInteractor.deleteAndBanUser(user)
         view.removeUserFromCurrentUI(user)
     }
 
     fun showContextMenuIfUserHasPermission(user: User) = scope.launch {
-        val currentUser = userRepository.getCurrentUser()
+        val currentUser = userListInteractor.getCurrentUser()
         if (user.role != Role.ADMIN.name &&
             currentUser.role == Role.ADMIN.name &&
             currentUser.userId != user.userId
@@ -52,6 +52,6 @@ class UserListPresenter @Inject constructor(
     }
 
     fun inviteUser() = scope.launch {
-        view.share(userRepository.getHouseholdIdForCurrentUser())
+        view.share(userListInteractor.getHouseholdIdForCurrentUser())
     }
 }
