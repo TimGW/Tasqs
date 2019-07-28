defb333f1639bf64d393370e710a2d461fb6c3f8
timgortworst <tim.gortworst@nos.nl>
2019-07-28 20:59:36 +0200

    empty list state and loading

--- app/src/main/java/com/timgortworst/roomy/repository/BaseResponse.kt
+++ app/src/main/java/com/timgortworst/roomy/repository/BaseResponse.kt
@@ -1,24 +1,12 @@
 package com.timgortworst.roomy.repository
 
 import com.google.firebase.firestore.DocumentChange
-import com.timgortworst.roomy.R
-import java.net.ConnectException
-import java.net.UnknownHostException
-
 
 abstract class BaseResponse {
 
     abstract fun renderSuccessfulState(dc: List<DocumentChange>?)
     abstract fun renderLoadingState()
     abstract fun renderUnsuccessfulState(throwable: Throwable)
-
-    fun determineErrorMessage(data: Throwable?): Int {
-        return when (data) {
-            null -> R.string.generic_error
-            is ConnectException, is UnknownHostException -> R.string.connection_error
-            else -> R.string.server_error
-        }
-    }
 
     fun setResponse(response: DataListener) {
         when (response) {
