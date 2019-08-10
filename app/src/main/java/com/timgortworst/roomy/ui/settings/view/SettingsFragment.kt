85c2d87095f3de8e50acfddcecfc470415f97275
timgortworst <tim.gortworst@nos.nl>
2019-08-10 12:41:06 +0200

    cleanup lint

--- app/src/main/java/com/timgortworst/roomy/ui/settings/view/SettingsFragment.kt
+++ app/src/main/java/com/timgortworst/roomy/ui/settings/view/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package com.timgortworst.roomy.ui.settings.view
 
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -12,12 +13,18 @@ import androidx.preference.SwitchPreferenceCompat
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.timgortworst.roomy.R
 import com.timgortworst.roomy.local.HuishoudGenootSharedPref
+import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     @Inject
     lateinit var sharedPref: HuishoudGenootSharedPref
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
