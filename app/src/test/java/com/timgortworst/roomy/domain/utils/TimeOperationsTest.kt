edd408f1a4700d9d2be8b9a235a8198c5365add1
timgortworst <tim.gortworst@nos.nl>
2020-02-22 16:25:36 +0100

    added some tests

--- app/src/test/java/com/timgortworst/roomy/domain/utils/TimeOperationsTest.kt
+++ app/src/test/java/com/timgortworst/roomy/domain/utils/TimeOperationsTest.kt
@@ -21,6 +21,102 @@ class TimeOperationsTest {
     }
 
     @Test
+    fun calcNextEvent_everyMonth() {
+        val result = timeOperations.nextEvent(baseInput, EventRecurrence.Monthly(1))
+        assertEquals(1, result.dayOfMonth)
+        assertEquals(2, result.month.value)
+
+        val resultNext = timeOperations.nextEvent(result, EventRecurrence.Monthly(1))
+        assertEquals(1, resultNext.dayOfMonth)
+        assertEquals(3, resultNext.month.value)
+
+        val resultLast = timeOperations.nextEvent(resultNext, EventRecurrence.Monthly(1))
+        assertEquals(1, resultLast.dayOfMonth)
+        assertEquals(4, resultLast.month.value)
+    }
+
+    @Test
+    fun calcNextEvent_everyOtherMonth() {
+        val result = timeOperations.nextEvent(baseInput, EventRecurrence.Monthly(2))
+        assertEquals(1, result.dayOfMonth)
+        assertEquals(3, result.month.value)
+
+        val resultNext = timeOperations.nextEvent(result, EventRecurrence.Monthly(2))
+        assertEquals(1, resultNext.dayOfMonth)
+        assertEquals(5, resultNext.month.value)
+
+        val resultLast = timeOperations.nextEvent(resultNext, EventRecurrence.Monthly(2))
+        assertEquals(1, resultLast.dayOfMonth)
+        assertEquals(7, resultLast.month.value)
+    }
+
+    @Test
+    fun calcNextEvent_everyDay() {
+        val result = timeOperations.nextEvent(baseInput, EventRecurrence.Daily(1))
+        assertEquals(2, result.dayOfMonth)
+        assertEquals(1, result.month.value)
+
+        val resultNext = timeOperations.nextEvent(result, EventRecurrence.Daily(1))
+        assertEquals(3, resultNext.dayOfMonth)
+        assertEquals(1, resultNext.month.value)
+
+        val resultLast = timeOperations.nextEvent(resultNext, EventRecurrence.Daily(1))
+        assertEquals(4, resultLast.dayOfMonth)
+        assertEquals(1, resultLast.month.value)
+    }
+
+    @Test
+    fun calcNextEvent_everyOtherDay() {
+        val result = timeOperations.nextEvent(baseInput, EventRecurrence.Daily(2))
+        assertEquals(3, result.dayOfMonth)
+        assertEquals(1, result.month.value)
+
+        val resultNext = timeOperations.nextEvent(result, EventRecurrence.Daily(2))
+        assertEquals(5, resultNext.dayOfMonth)
+        assertEquals(1, resultNext.month.value)
+
+        val resultLast = timeOperations.nextEvent(resultNext, EventRecurrence.Daily(2))
+        assertEquals(7, resultLast.dayOfMonth)
+        assertEquals(1, resultLast.month.value)
+    }
+
+    @Test
+    fun calcNextEvent_everyYear() {
+        val result = timeOperations.nextEvent(baseInput, EventRecurrence.Annually(1))
+        assertEquals(1, result.dayOfMonth)
+        assertEquals(1, result.month.value)
+        assertEquals(2021, result.year)
+
+        val resultNext = timeOperations.nextEvent(result, EventRecurrence.Annually(1))
+        assertEquals(1, resultNext.dayOfMonth)
+        assertEquals(1, resultNext.month.value)
+        assertEquals(2022, resultNext.year)
+
+        val resultLast = timeOperations.nextEvent(resultNext, EventRecurrence.Annually(1))
+        assertEquals(1, resultLast.dayOfMonth)
+        assertEquals(1, resultLast.month.value)
+        assertEquals(2023, resultLast.year)
+    }
+
+    @Test
+    fun calcNextEvent_everyOtherYear() {
+        val result = timeOperations.nextEvent(baseInput, EventRecurrence.Annually(2))
+        assertEquals(1, result.dayOfMonth)
+        assertEquals(1, result.month.value)
+        assertEquals(2022, result.year)
+
+        val resultNext = timeOperations.nextEvent(result, EventRecurrence.Annually(2))
+        assertEquals(1, resultNext.dayOfMonth)
+        assertEquals(1, resultNext.month.value)
+        assertEquals(2024, resultNext.year)
+
+        val resultLast = timeOperations.nextEvent(resultNext, EventRecurrence.Annually(2))
+        assertEquals(1, resultLast.dayOfMonth)
+        assertEquals(1, resultLast.month.value)
+        assertEquals(2026, resultLast.year)
+    }
+
+    @Test
     fun calcNextWeekDay_everySunday() {
         val weekDays = listOf(7)
 
