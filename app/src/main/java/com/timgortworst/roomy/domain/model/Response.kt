29c26bc2b6e105fa505cb77550e3995762cea2ab
timgortworst <tim.gortworst@nos.nl>
2020-03-27 16:33:25 +0100

    removed classes

--- app/src/main/java/com/timgortworst/roomy/domain/model/Response.kt
+++
@@ -1,7 +0,0 @@
-package com.timgortworst.roomy.domain.model
-
-sealed class Response {
-    object Loading : Response()
-    data class Error(val e: Throwable?) : Response()
-    data class Success<out T>(val data: T): Response()
-}
--- app/src/main/java/com/timgortworst/roomy/presentation/features/settings/SettingsView.kt
+++
@@ -1,7 +0,0 @@
-package com.timgortworst.roomy.presentation.features.settings
-
-import androidx.annotation.StringRes
-
-interface SettingsView {
-    fun easterEggMsg(@StringRes stringRes: Int, argument: Int? = null)
-}
