c09465305d3f95d566e051168ab031066b606d27
tim91g <tim.gortworst@nos.nl>
2020-04-01 11:15:37 +0200

    default datetime of noon

--- app/src/main/java/com/timgortworst/roomy/domain/model/TaskMetaData.kt
+++ app/src/main/java/com/timgortworst/roomy/domain/model/TaskMetaData.kt
@@ -3,11 +3,19 @@ package com.timgortworst.roomy.domain.model
 import android.os.Parcelable
 import com.google.firebase.firestore.IgnoreExtraProperties
 import kotlinx.android.parcel.Parcelize
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalTime
+import org.threeten.bp.ZoneId
 import org.threeten.bp.ZonedDateTime
 
 @Parcelize
 @IgnoreExtraProperties
 data class TaskMetaData(
-        var startDateTime: ZonedDateTime = ZonedDateTime.now(),
-        var recurrence: TaskRecurrence = TaskRecurrence.SingleTask(1)
+    var startDateTime: ZonedDateTime = ZonedDateTime.of(
+        LocalDate.now(),
+        LocalTime.NOON,
+        ZoneId.systemDefault()
+    ),
+    var recurrence: TaskRecurrence = TaskRecurrence.SingleTask(1)
 ) : Parcelable
+
