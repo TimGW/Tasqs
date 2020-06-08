1acd274b0dd9e929d2af2d503ee22879d6a73e4f
Gortworst <tim.gortworst@nos.nl>
2020-06-08 09:00:27 +0200

    update remove channelflow

--- app/src/main/java/com/timgortworst/roomy/domain/usecase/signin/SignInUseCaseImpl.kt
+++ app/src/main/java/com/timgortworst/roomy/domain/usecase/signin/SignInUseCaseImpl.kt
@@ -7,14 +7,10 @@ import com.timgortworst.roomy.domain.model.response.ErrorHandler
 import com.timgortworst.roomy.domain.model.response.Response
 import com.timgortworst.roomy.domain.repository.HouseholdRepository
 import com.timgortworst.roomy.domain.repository.UserRepository
-import com.timgortworst.roomy.presentation.RoomyApp
 import com.timgortworst.roomy.presentation.usecase.signin.SignInUseCase
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 class SignInUseCaseImpl(
@@ -27,18 +23,17 @@ class SignInUseCaseImpl(
 
     data class Params(val newUser: Boolean)
 
-    override fun execute(params: Params?) = channelFlow {
+    override fun execute(params: Params?) = flow {
         checkNotNull(params)
 
-        val loadingJob = CoroutineScope(coroutineContext).launch {
-            delay(RoomyApp.LOADING_DELAY)
-            offer(Response.Loading)
-        }
+        emit(Response.Loading)
 
-        val fireBaseUser = fbAuth.currentUser ?: run { offer(Response.Error()); return@channelFlow }
-        val token = fbInstanceId.instanceId.await().token
+        val fireBaseUser = fbAuth.currentUser ?: run { emit(Response.Error()); return@flow }
 
         try {
+            val instanceId = fbInstanceId.instanceId.await()
+            val token = instanceId.token
+
             if (params.newUser) {
                 val householdId = householdRepository.createHousehold()
                 userRepository.createUser(householdId, fireBaseUser, token)
@@ -46,11 +41,9 @@ class SignInUseCaseImpl(
                 userRepository.addUserToken(fireBaseUser.uid, token)
             }
             userRepository.getUser() // get user to update cache
-            offer(Response.Success(fireBaseUser.displayName.orEmpty()))
+            emit(Response.Success(fireBaseUser.displayName.orEmpty()))
         } catch (e: FirebaseFirestoreException) {
-            offer(Response.Error(errorHandler.getError(e)))
-        } finally {
-            loadingJob.cancel()
+            emit(Response.Error(errorHandler.getError(e)))
         }
     }.flowOn(Dispatchers.IO)
 }
