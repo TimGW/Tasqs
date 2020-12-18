1ef764674dd103573bf7cadc35d9a986533344d7
Tim Gortworst <tim.gortworst@gmail.com>
2020-12-18 13:53:55 +0100

    remove recurring worker code

--- app/src/main/java/com/timgortworst/tasqs/infrastructure/notifications/NotificationWorker.kt
+++ app/src/main/java/com/timgortworst/tasqs/infrastructure/notifications/NotificationWorker.kt
@@ -1,21 +1,17 @@
 package com.timgortworst.tasqs.infrastructure.notifications
 
 import android.content.Context
-import androidx.work.*
+import androidx.work.Worker
+import androidx.work.WorkerParameters
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.timgortworst.tasqs.R
-import org.koin.android.ext.android.inject
 import org.koin.core.KoinComponent
 import org.koin.core.inject
-import org.threeten.bp.Duration
-import org.threeten.bp.ZonedDateTime
-import java.util.concurrent.TimeUnit
 
 class NotificationWorker(
     private val context: Context,
     params: WorkerParameters
 ) : Worker(context, params), KoinComponent {
-    private val workManager = WorkManager.getInstance(context)
     private val notifications: Notifications by inject()
 
     override fun doWork() = try {
@@ -31,27 +27,9 @@ class NotificationWorker(
             context.getString(R.string.notification_message, text)
         )
 
-        setTomorrowReminder(id)
-
         Result.success()
     } catch (e: Exception) {
         FirebaseCrashlytics.getInstance().recordException(e)
         Result.failure()
-    }
-
-    private fun setTomorrowReminder(
-        id: String
-    ) {
-        val now = ZonedDateTime.now()
-        val nextReminder = now.plusDays(1)
-        val initialDelay = Duration.between(now, nextReminder).toMillis()
-
-        val workRequest = OneTimeWorkRequestBuilder<NotificationWorker>()
-            .setInitialDelay(initialDelay, TimeUnit.MILLISECONDS)
-            .addTag(id)
-            .setInputData(inputData)
-            .build()
-
-        workManager.enqueueUniqueWork(id, ExistingWorkPolicy.REPLACE, workRequest)
     }
 }
